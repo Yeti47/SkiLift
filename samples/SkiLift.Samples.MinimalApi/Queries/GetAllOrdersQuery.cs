f2c71bb2ba2f05f180533544b5a162e51eef91af
Yeti47 <15141246+Yeti47@users.noreply.github.com>
2025-04-10 21:23:00 +0200

    Refactor: Simplified GetAllOrdersHandler by removing unnecessary field

--- samples/SkiLift.Samples.MinimalApi/Queries/GetAllOrdersQuery.cs
+++ samples/SkiLift.Samples.MinimalApi/Queries/GetAllOrdersQuery.cs
@@ -10,10 +10,8 @@ public class GetAllOrdersQuery : IQuery<IEnumerable<Order>>
 
 public class GetAllOrdersHandler(IOrderRepository orderRepository) : IQueryHandler<GetAllOrdersQuery, IEnumerable<Order>>
 {
-    private readonly IOrderRepository _orderRepository = orderRepository;
-
     public Task<IEnumerable<Order>> Handle(GetAllOrdersQuery request, CancellationToken cancellationToken)
     {
-        return _orderRepository.GetAllAsync();
+        return orderRepository.GetAllAsync();
     }
 }
